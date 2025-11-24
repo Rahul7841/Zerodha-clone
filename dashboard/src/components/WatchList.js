bb5b73493a32b44bc1670a0dd30e948e435f675e
Rahul Shelke <rahulshelke0011@gmail.com>
2025-11-24 15:30:46 +0530

    basic setup done for dashboard

---
+++ dashboard/src/components/WatchList.js
@@ -0,0 +1,19 @@
+
+ const WatchList = () => {
+  return (
+    <div className="watchlist-container">
+      <div className="search-container">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search eg:infy, bse, nifty fut weekly, gold mcx"
+          className="search"
+        />
+        <span className="counts"> / 50</span>
+      </div>
+    </div>
+  );
+};
+
+export default WatchList;
